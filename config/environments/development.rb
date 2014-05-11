8af5a0f903e7690e53a7376d156a04b7f685389b
Sachin Patel <sachb52@gmail.com>
2014-05-11 12:42:58 -0400

    card

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,8 +22,10 @@ DemoApp::Application.configure do
   # Raise an error on page load if there are pending migrations
   config.active_record.migration_error = :page_load
 
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.serve_static_assets = false
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ DemoApp::Application.routes.draw do
   resources :users
 
   root to: "users#index"
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
